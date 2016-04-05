fc50685d59949940f59b8c6bb2ed37420316f117
jorelstudent <lpena006@ucr.edu>
2016-04-04 17:01:56 -0700

    Delete ex3a.py

--- Code/Python/week01/ex3a.py
+++
@@ -1,7 +0,0 @@
-## import package for square roots
-import math
-
-h = 800
-v_i = input('Input the initial velocity [m/s]: ') 
-t = (-v_i / 9.81) + math.sqrt(v_i ** 2 + 2 * h * 9.81) / 9.81
-print 'Time [s] it takes to reach the ground: ', t
